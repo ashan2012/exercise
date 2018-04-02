c5babc5f108115bd8bf17f8abf8fb58b48495379
weitao <weitao@meituan.com>
2018-04-02 11:11:47 +0800

    add softmax and backward and forward demo

---
+++ pythonDemo/tensorflowDemo/baseDemo/HelloWord.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/31 下午12:27
+# @Author  : weitao
+# @Site    : 
+# @File    : HelloWord.py
+# @descrip :
+# @Software: PyCharm Community Edition
